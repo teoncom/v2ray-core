jefferymvp/sagernet-core

- removed secureload, use filesystem.ReadFile directly

app/browserforwarder/forwarder.go
@@ -6,8 +6,10 @@ package browserforwarder
 import (
 	"bytes"
 	"context"
+	"github.com/v2fly/v2ray-core/v4/common/platform"
 	"io"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -15,7 +17,7 @@ import (
 
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/net"
-	"github.com/v2fly/v2ray-core/v4/common/platform/securedload"
+	"github.com/v2fly/v2ray-core/v4/common/platform/filesystem"
 	"github.com/v2fly/v2ray-core/v4/features/extension"
 	"github.com/v2fly/v2ray-core/v4/transport/internet"
 )
@@ -111,7 +113,10 @@ func BridgeResource(rw http.ResponseWriter, r *http.Request, path string) {
 	if content == "" {
 		content = "index.html"
 	}
-	data, err := securedload.GetAssetSecured("browserforwarder/" + content)
+
+	platformFileName := filepath.FromSlash("browserforwarder/" + content)
+	data, err := filesystem.ReadFile(platform.GetAssetLocation(platformFileName))
+
 	if err != nil {
 		err = newError("cannot load necessary resources").Base(err)
 		http.Error(rw, err.Error(), http.StatusForbidden)

teoncom/v2ray-core

app/browserforwarder/forwarder.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -12,7 +13,8 @@ import (
 
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/common/net"
-	"github.com/v2fly/v2ray-core/v5/common/platform/securedload"
+	"github.com/v2fly/v2ray-core/v5/common/platform"
+	"github.com/v2fly/v2ray-core/v5/common/platform/filesystem"
 	"github.com/v2fly/v2ray-core/v5/features/extension"
 	"github.com/v2fly/v2ray-core/v5/transport/internet"
 )
@@ -108,7 +110,9 @@ func BridgeResource(rw http.ResponseWriter, r *http.Request, path string) {
 	if content == "" {
 		content = "index.html"
 	}
-	data, err := securedload.GetAssetSecured("browserforwarder/" + content)
+
+	platformFileName := filepath.FromSlash("browserforwarder/" + content)
+	data, err := filesystem.ReadFile(platform.GetAssetLocation(platformFileName))
 	if err != nil {
 		err = newError("cannot load necessary resources").Base(err)
 		http.Error(rw, err.Error(), http.StatusForbidden)