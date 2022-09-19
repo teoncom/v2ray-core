34bc99a04074cd7473d48bc522e386680b4e5352
AllenHu <AllenHu@Teon.com>
2022-09-19 08:56:33 +0800

    update Build.bat

--- Build.bat
+++ Build.bat
@@ -2,5 +2,5 @@ set GOOS=windows
 set CGO_ENABLED=1
 set GOARCH=amd64
 go mod tidy
-go build -a -trimpath -asmflags "-s -w" -ldflags "-s -w -buildid=" -buildmode=c-shared -o "..\..\release\v2ray-sn.dll" ".\main"
+go build -a -trimpath -asmflags "-s -w" -ldflags "-s -w -buildid=" -buildmode=c-shared -o ".\bin\v2ray-sn.dll" ".\main"
 pause
